cc10473004d394e2e75cd308ba0cb7e682996be4
Rita Bastos <rita.cbastos21@gmail.com>
2017-10-09 18:07:30 +0100

    4v

--- src/org/academiadecodigo/javabank/Controller/BalanceController.java
+++ src/org/academiadecodigo/javabank/Controller/BalanceController.java
@@ -5,11 +5,15 @@ import org.academiadecodigo.javabank.View.View;
 public class BalanceController {
 
     private View view;
+    private MenuController nextController2;
 
 
     public void start(){
         view.show();
     }
+
+    //public void
+    //nextController2.start();
 
 
 }
